8bc3f4957df38e32081d57ba271b37e88e6d3599
Viktoriia Ulianova <viktoriia.ulianova@gateway.fm>
2025-04-17 20:04:28 +0400

    docker image: 1.1.4

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import checker from "vite-plugin-checker";
+import { checker } from "vite-plugin-checker";
 import svgr from "vite-plugin-svgr";
 
 // eslint-disable-next-line import/no-default-export
