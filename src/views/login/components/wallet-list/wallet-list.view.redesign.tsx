7d8ebfd2d03dcd1884a69bb6e0052e18ed9db76b
Mikhail Makeev <mihail.makeev@gmail.com>
2025-10-21 13:52:55 +0300

    fix(wallet-list): update environment display to use dynamic chain name

--- src/views/login/components/wallet-list/wallet-list.view.redesign.tsx
+++ src/views/login/components/wallet-list/wallet-list.view.redesign.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 
 import { WalletIconRedesign } from "../wallet-icon/wallet-icon.view.redesign";
 import CornerWhiteIcon from "src/assets/icons/corner-right-white.svg?react";
+import { useEnvContext } from "src/contexts/env.context";
 import { WalletName } from "src/domain";
 import { useWalletListRedesignStyles } from "src/views/login/components/wallet-list/wallet-list.styles";
 import { CardRedesign } from "src/views/shared/card/card.view.redesign";
@@ -14,12 +15,14 @@ type WalletListProps = {
 export const WalletListRedesign: FC<WalletListProps> = ({ onSelectWallet }) => {
   const classes = useWalletListRedesignStyles();
   const isMobile = window.innerWidth < 788;
+  const env = useEnvContext();
+  const ethereumChain = env?.chains[0];
 
   return (
     <CardRedesign>
       <div className={classes.cardBox}>
         <Typography type="h1">Connect a wallet</Typography>
-        <div className={classes.smallTitle}>Connect with Sepolia Testnet environment</div>
+        <div className={classes.smallTitle}>Connect with {ethereumChain?.name} environment</div>
         <ul className={classes.walletList}>
           <li
             className={classes.wallet}
